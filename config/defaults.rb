4b308f5430ab3dc304d9949af2ee726e7b3318a7
Chris Scheper <prpht9@gmail.com>
2023-05-14 11:50:57 -0400

    add group configuration

--- config/defaults.rb
+++ config/defaults.rb
@@ -101,6 +101,166 @@ DEFAULTS = {
         :lab
       ]
     }]
-  ]
+  ],
+  :group_strategy => {
+    :team_small => {
+      :name => "Small Team Group Naming Strategy",
+      :description => "Keep It Simple Stupid, no frills, just basic groups to drop everyone into.",
+      :size => 'small',
+      :tags => [
+        :team,
+        :small,
+        :generic_support,
+        :single_finance_group,
+        :network_in_platform,
+        :infrstructure_in_platform,
+        :devops_in_platform,
+        :executive_in_management,
+      ],
+      :names => [
+        'engineering',
+        'platform',
+        'product',
+        'security',
+        'support',
+        'management',
+        'hr',
+        'marketing',
+        'sales',
+        'finance'
+      ]
+    },
+    :team_medium => {
+      :name => "Medium Team Group Naming Strategy",
+      :description => "Keep It Simple Stupid, no frills, just basic groups to drop everyone into.",
+      :size => 'medium',
+      :tags => [
+        :team,
+        :medium,
+        :generic_support,
+        :two_finance_group,
+        :network_and_dba,
+        :qa_in_engineering,
+        :infrstructure_in_platform,
+        :sre_in_platform,
+        :devops_in_platform,
+        :executive_and_management,
+      ],
+      :names => [
+        'engineering',
+        'platform',
+        'dba',
+        'network',
+        'product',
+        'security',
+        'support',
+        'management',
+        'executive',
+        'hr',
+        'marketing',
+        'sales',
+        'auditor',
+        'finance'
+      ]
+    },
+    :team_large => {
+      :name => "Large Team Group Naming Strategy",
+      :description => "Keep It Simple Stupid, no frills, just basic groups to drop everyone into.",
+      :size => 'large',
+      :tags => [
+        :team,
+        :large,
+        :network_and_dba,
+        :sre,
+        :separated_finance,
+        :separated_platform,
+        :separated_support,
+        :executive_and_management
+      ],
+      :names => [
+        'engineering',
+        'platform',
+        'dba',
+        'sre',
+        'network',
+        'qa',
+        'product',
+        'security',
+        'csr',
+        'tech_support',
+        'management',
+        'executive',
+        'hr',
+        'marketing',
+        'sales',
+        'fin_admin',
+        'fin_ar',
+        'fin_ap',
+        'fin_corp',
+        'fin_auditor'
+      ]
+    },
+    :responsibility => {
+      :name => "Responsibility Group Naming Strategy",
+      :description => "This model can be confusing if you also use action based roles.",
+      :size => 'large',
+      :tags => [
+        :responsibility,
+        :large,
+        :admin,
+        :cloud,
+        :user,
+        :security
+      ],
+      :names => [
+        "cloud-admin",
+        "cloud-developer",
+        "cloud-user",
+        "cloud-viewer",
+        "network-admin",
+        "security-admin"
+      ]
+    },
+  },
+  :role_strateegy => {
+    :action => [
+      'compute_admin',
+      'compute_user',
+      'compute_viewer',
+      'k8s_admin',
+      'k8s_user',
+      'k8s_viewer',
+      'data_admin',
+      'data_user',
+      'data_viewer',
+      'log_admin',
+      'log_user',
+      'log_viewer',
+      'iam_admin',
+      'iam_user',
+      'iam_viewer',
+      'vault_admin',
+      'vault_user',
+      'vault_viewer',
+      'storage_admin',
+      'storage_user',
+      'storage_viewer',
+      'network_admin',
+      'network_user',
+      'network_viewer'
+    ],
+    :env => [
+      'stg_atlassian_admin',
+      'stg_k8s_admin',
+      'stg_k8s_tools_admin',
+      'stg_compute_admin',
+      'stg_data_warehouse_admin',
+      'stg_data_warehouse_user',
+      'perf_admin',
+      'perf_developer',
+      'perf_gcs_admin',
+      'perf_gcs_viewer'
+    ]
+  }
 }
 
